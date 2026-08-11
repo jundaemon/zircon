00a88f8a824ca87f60ec5571e79076aada253aef
Jun Wei <junweilee72@gmail.com>
2026-08-12 03:43:33 +0800

    minor change

--- src/function.zig
+++ src/function.zig
@@ -3,21 +3,21 @@ const math = std.math;
 
 pub const Activation = enum { None, Tanh, ReLU };
 
-pub fn MSE(comptime out_size: usize, predicted: [out_size]f32, expected: [out_size]f32) f32 {
-    if (out_size < 1) @compileError("out_size should be 1 or more");
+pub fn MSE(comptime out: usize, predicted: [out]f32, expected: [out]f32) f32 {
+    if (out < 1) @compileError("out_size should be 1 or more");
 
     var mse: f32 = 0;
-    for (0..out_size) |i| mse += math.pow(f32, predicted[i] - expected[i], 2);
-    mse /= out_size;
+    for (0..out) |i| mse += math.pow(f32, predicted[i] - expected[i], 2);
+    mse /= out;
 
     return mse;
 }
 
-pub fn MSE_grad(comptime out_size: usize, predicted: [out_size]f32, expected: [out_size]f32) [out_size]f32 {
-    if (out_size < 1) @compileError("out_size should be 1 or more");
+pub fn MSE_grad(comptime out: usize, predicted: [out]f32, expected: [out]f32) [out]f32 {
+    if (out < 1) @compileError("out_size should be 1 or more");
 
-    var mse_grad: [out_size]f32 = undefined;
-    for (0..out_size) |i| mse_grad[i] = (predicted[i] - expected[i]) * 2 / out_size;
+    var mse_grad: [out]f32 = undefined;
+    for (0..out) |i| mse_grad[i] = (predicted[i] - expected[i]) * 2 / out;
 
     return mse_grad;
 }
